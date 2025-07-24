a8ed178a51083cfc0cf2a185853539eac63494ae
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-07-23 21:35:26 -0300

    Aula de quarta-feira 23/07/2025

---
+++ src/paginas/Home/index.js
@@ -0,0 +1,32 @@
+import Cabecalho from "../../componentes/Cabecalho";
+import Cartao from "../../componentes/Cartao";
+import Rodape from "../../componentes/Rodape";
+
+function Home() {
+    return (
+        <>
+            <Cabecalho />
+
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-3 col-6">
+                        <Cartao />
+                    </div>
+                    <div className="col-md-3 col-6">
+                        <Cartao />
+                    </div>
+                    <div className="col-md-3 col-6">
+                        <Cartao />
+                    </div>
+                    <div className="col-md-3 col-6">
+                        <Cartao />
+                    </div>
+                </div>
+            </div>
+
+            <Rodape />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/paginas/Login/index.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import logo from "../../assets/treina_recife_logo.jpeg";
+import "./login.css";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [manterConectado, setManterConectado] = useState(true);
+
+    const enviarFormulario = () => {
+        console.log({ email, senha, manterConectado });
+    }
+
+    return (
+        <div className="container-login">
+            <img src={logo} alt="Treina Recife" width="250px" className="mb-5" />
+            <div className="col-4">
+                <div className="mb-3">
+                    <label htmlFor="email-input" className="form-label">E-mail</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        id="email-input" 
+                        placeholder="nome@exemplo.com" 
+                        defaultValue={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="senha-input" className="form-label">Senha</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="senha-input" 
+                        defaultValue={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                </div>
+                <div className="form-check mb-3">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="check-input"
+                        defaultChecked={manterConectado}
+                        onClick={() => setManterConectado(!manterConectado)}
+                    />
+                    <label className="form-check-label" htmlFor="check-input">
+                        Mantenha-me conectado
+                    </label>
+                </div>
+
+                <button 
+                    type="button" 
+                    className="btn btn-primary mb-3 w-100"
+                    onClick={enviarFormulario}
+                >
+                    Entrar
+                </button>
+            </div>
+            
+            <a className="form-link" href="https://www.treinarecife.com.br/" target="_blank">Esqueci a senha</a>
+        </div>
+    )
+}
+
+export default Login;
